1f34010777178859ea3b01f1d295a18ec9ed1d8b
Matthew McKey <mcstaplez@gmail.com>
2020-06-26 17:49:44 +0000

    Upload: js/rollup.config.js

--- js/rollup.config.js
+++ js/rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 
 export default {
-    input: 'src/app.js',
+    input: './app.js',
     output: {
         file: '../scripts/artjam.js',
         format: "cjs"
