5fdd7e88c9a61c30c4a9e3dc911d5a605f8fc070
Matthew McKey <mcstaplez@gmail.com>
2020-06-26 12:30:09 +0000

    Upload: js/src/app.js

--- js/src/app.js
+++ js/src/app.js
@@ -2,6 +2,19 @@
 	const isJamPage = window.location.pathname === '/jam';
 	if (!isJamPage) return;
 
-	const votes = {};
+	const votes = [];
 
+	// helpers
+
+	const toggleVote = id => {
+
+	};
+
+	const elementIdToVoteId = id => id.replace('yui_', '');
+
+	// event listeners
+
+	$(document).on('click', '.vote-button', function (e) {
+
+	});
 })(window)
