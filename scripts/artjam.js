a8dc8308166a01fbeda569be9c70d975f61a425a
Matthew McKey <mcstaplez@gmail.com>
2020-06-26 16:22:18 +0000

    Upload: scripts/artjam.js

--- scripts/artjam.js
+++ scripts/artjam.js
@@ -1,8 +1,45 @@
 'use strict';
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
+  try {
+    var info = gen[key](arg);
+    var value = info.value;
+  } catch (error) {
+    reject(error);
+    return;
+  }
+
+  if (info.done) {
+    resolve(value);
+  } else {
+    Promise.resolve(value).then(_next, _throw);
+  }
+}
+
+function _asyncToGenerator(fn) {
+  return function () {
+    var self = this,
+        args = arguments;
+    return new Promise(function (resolve, reject) {
+      var gen = fn.apply(self, args);
+
+      function _next(value) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
+      }
+
+      function _throw(err) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+      }
+
+      _next(undefined);
+    });
+  };
+}
+
 (function (window) {
   var isJamPage = window.location.pathname === '/jam';
   if (!isJamPage) return;
+  var API_URL = 'https://artjam.ngrok.io';
   var buttonTemplate = "<button class=\"voting-button\">&uarr; SELECT &uarr;</button>";
   var votes = []; // helpers
 
@@ -38,10 +75,43 @@
     return id.replace('yui_', '');
   };
 
-  var submitVotes = function submitVotes(submittedVotes) {
-    var voteJSON = JSON.stringify(submittedVotes);
-    console.log(voteJSON);
-  }; // event listeners
+  var submitVotes = /*#__PURE__*/function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(submittedVotes) {
+      var voteJSON, history, response;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              voteJSON = JSON.stringify(submittedVotes);
+              history = window.history;
+              _context.next = 4;
+              return fetch("".concat(API_URL, "/votes"), {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                redirect: 'follow',
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify(votes)
+              }).json();
+
+            case 4:
+              response = _context.sent;
+
+            case 5:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee);
+    }));
+
+    return function submitVotes(_x) {
+      return _ref.apply(this, arguments);
+    };
+  }(); // event listeners
 
 
   $(document).on('click', '.voting-button', function (e) {
