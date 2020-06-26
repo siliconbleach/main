af70cef4ce83192c1465ca8e876acb38d354365d
Matthew McKey <mcstaplez@gmail.com>
2020-06-26 12:33:30 +0000

    Upload: scripts/artjam.js

--- scripts/artjam.js
+++ scripts/artjam.js
@@ -4,4 +4,31 @@
   var isJamPage = window.location.pathname === '/jam';
   if (!isJamPage) return;
 
+  var elementIdToVoteId = function elementIdToVoteId(id) {
+    return id.replace('yui_', '');
+  }; // event listeners
+
+
+  $(document).on('click', '.vote-button', function (e) {
+    var $slide = $(this).parent();
+    var voteId = elementIdToVoteId($slide.attr('id'));
+    console.log(voteId);
+  });
+})(window);
+'use strict';
+
+(function (window) {
+  var isJamPage = window.location.pathname === '/jam';
+  if (!isJamPage) return;
+
+  var elementIdToVoteId = function elementIdToVoteId(id) {
+    return id.replace('yui_', '');
+  }; // event listeners
+
+
+  $(document).on('click', '.vote-button', function (e) {
+    var $slide = $(this).parent();
+    var voteId = elementIdToVoteId($slide.attr('id'));
+    console.log(voteId);
+  });
 })(window);
