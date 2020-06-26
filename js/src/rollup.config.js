5d3cc4220f0c18a10ed0f38112d1f579a646d06b
Matthew McKey <mcstaplez@gmail.com>
2020-06-26 17:50:12 +0000

    Upload: js/src/rollup.config.js

---
+++ js/src/rollup.config.js
@@ -0,0 +1,14 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+
+export default {
+    input: './app.js',
+    output: {
+        file: '../scripts/artjam.js',
+        format: "cjs"
+    },
+    plugins: [
+        resolve(),
+        babel({ babelHelpers: 'bundled' })
+    ]
+}
