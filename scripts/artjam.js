82bc139ccef7651d6493a4ceb1bf73562dc4911e
Matthew McKey <mcstaplez@gmail.com>
2020-06-26 12:34:07 +0000

    Upload: scripts/artjam.js

--- scripts/artjam.js
+++ scripts/artjam.js
@@ -12,6 +12,5 @@
   $(document).on('click', '.vote-button', function (e) {
     var $slide = $(this).parent();
     var voteId = elementIdToVoteId($slide.attr('id'));
-    console.log('vote id is:' + voteId);
   });
 })(window);
