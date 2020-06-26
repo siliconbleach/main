55d4bf967c10e55c58cc5c0936169f7829158161
Matthew McKey <mcstaplez@gmail.com>
2020-06-26 12:55:56 +0000

    Upload: scripts/artjam.js

--- scripts/artjam.js
+++ scripts/artjam.js
@@ -18,7 +18,11 @@
 
     if (typeof vote !== 'undefined') {
       votes.push(vote);
+    } else {
+      votes.splice(vote, 1);
     }
+
+    $voteSlide.toggleClass('slide-seiected');
   };
 
   var elementIdToVoteId = function elementIdToVoteId(id) {
