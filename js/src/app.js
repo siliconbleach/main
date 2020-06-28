5adfb2cf81621bfdafe983a34a92ccad653c11b4
Matthew McKey <mcstaplez@gmail.com>
2020-06-28 18:21:40 +0000

    Upload: js/src/app.js

--- js/src/app.js
+++ js/src/app.js
@@ -73,4 +73,79 @@ const { default: Axios } = require("axios");
 			votes = storedSettings.votes;
 		}
 	});
+})(window)const { default: Axios } = require("axios");
+
+((window) => {
+	const isJamPage = window.location.pathname === '/jam';
+	const hasCookie = document.cookie.split('; ').find(row => row.startsWith('artjam_admin'));
+	if (!isJamPage) return;
+	const API_URL = 'https://artjam.ngrok.io';
+
+	const buttonTemplate = `<button class="voting-button">&uarr; SELECT &uarr;</button>`;
+
+	let votes = [];
+
+	// helpers
+
+	const toggleVote = id => {
+
+		const $voteSlide = $(`#yui_${id}`);
+		let styles = {
+			background: 'white',
+			color: '#e86d6d'
+		}
+
+		if (votes.length > 4) {
+			return alert('You can only vote five times.');
+		}
+
+		const vote = votes.find(v => v === id);
+
+		if (typeof vote !== 'undefined') {
+			votes.splice(vote, 1);
+			styles = {
+				background: 'transparent',
+				color: '#fff'
+			}
+		} else {
+			votes.push(id);
+		}
+
+
+		$voteSlide.find('.voting-button').toggleClass('is-seiected').css(styles)
+	};
+
+	const elementIdToVoteId = id => id.replace('yui_', '');
+
+	const submitVotes = async submittedVotes => {
+		const voteJSON = JSON.stringify(submittedVotes);
+		const response = Axios.post(`${API_URL}/api/votes`, {
+			votes
+		}).then(res => res.json()).catch(e => console.log({ e }));
+	}
+
+	// event listeners
+
+	$(document).on('click', '.voting-button', function (e) {
+		const $slide = $(this).parent();
+		const voteId = elementIdToVoteId($slide.attr('id'));
+
+		toggleVote(voteId);
+	});
+
+
+	$(document).on('click', '#submitvotes-button', e => {
+		e.preventDefault();
+		submitVotes(votes);
+	});
+
+	$(document).on('ready', function () {
+		$('.slide').append(buttonTemplate);
+
+		const retrieveStoredSettings = window.localStorage.getItem('artJamInfo');
+		if (typeof retrieveStoredSettings === 'string') {
+			const storedSettings = JSON.parse(retrieveVotesFromStorage);
+			votes = storedSettings.votes;
+		}
+	});
 })(window)
