8e5d722431bb4d0f6ed110907489e6b667d04e75
Matthew McKey <mcstaplez@gmail.com>
2020-07-01 11:01:45 +0000

    Upload: js/src/rollup.config.js

--- js/src/rollup.config.js
+++ js/src/rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-
+import json from '@rollup/plugin-json'
 export default {
     input: './app.js',
     output: {
