b79b750969cb034701cf0b2318cb5026195d30f2
Matthew McKey <mcstaplez@gmail.com>
2020-07-01 00:16:57 +0000

    Upload: scripts/artjam.js

--- scripts/artjam.js
+++ scripts/artjam.js
@@ -184,6 +184,7 @@ var js_cookie = createCommonjsModule(function (module, exports) {
   });
   if (!isJamPage) return;
   var API_URL = 'https://artjam.ngrok.io';
+  var buttonTemplate = "<button class=\"voting-button\">&uarr; SELECT &uarr;</button>";
   var votes = []; // helpers
 
   var toggleVote = function toggleVote(id) {
@@ -238,7 +239,7 @@ var js_cookie = createCommonjsModule(function (module, exports) {
     submitVotes(votes);
   });
   $(document).on('ready', function () {
-    // $('.slide').append(buttonTemplate);
+    $('.slide').append(buttonTemplate);
     var cookieCheck = js_cookie.get('');
     var retrieveStoredSettings = window.localStorage.getItem('artJamInfo');
 
