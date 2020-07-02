1478da825b0154921fcb8dc0c28430e90f07b104
Matthew McKey <mcstaplez@gmail.com>
2020-07-02 16:36:47 +0000

    Upload: scripts/artjam.js

--- scripts/artjam.js
+++ scripts/artjam.js
@@ -306,9 +306,9 @@ var js_cookie = createCommonjsModule(function (module, exports) {
               return fetch("".concat(API_URL, "/api/votes"), {
                 method: 'POST',
                 body: settings,
-                headers: [{
+                headers: {
                   'Content-Type': 'application/json'
-                }]
+                }
               }).then(function (res) {
                 return res.json();
               }).then(function (data) {
