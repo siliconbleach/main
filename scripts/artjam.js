a0e5c4437aa58e63d8f208b1d7c78465103cbf1e
Matthew McKey <mcstaplez@gmail.com>
2020-07-02 10:02:44 +0000

    Upload: scripts/artjam.js

--- scripts/artjam.js
+++ scripts/artjam.js
@@ -351,7 +351,7 @@ var js_cookie = createCommonjsModule(function (module, exports) {
         console.log(votes);
         settings.votes = votes;
         settings.votes.forEach(function (vote) {
-          return document.getElementById(vote.piece_id).querySelector('.voting-button').toggleClass('is-selected');
+          return document.getElementById("".concat(YUI_PREFIX).concat(vote.piece_id)).querySelector('.voting-button').toggleClass('is-selected');
         });
       });
     }
