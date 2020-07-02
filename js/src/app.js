9ce65179ac11d3bbd99de9c003f44e9ab1215c62
Matthew McKey <mcstaplez@gmail.com>
2020-07-02 10:24:43 +0000

    Upload: js/src/app.js

--- js/src/app.js
+++ js/src/app.js
@@ -79,7 +79,7 @@ import cookie from 'js-cookie';
 	$(document).on('ready', function () {
 		const $slides = Array.from(document.querySelectorAll('.slide'));
 		yui_gallery_id = $slides[0].id.split('_');
-		yui_gallery_id = yui_gallery_id.slice(1, yui_gallery_id.length - 2).join('_');
+		yui_gallery_id = yui_gallery_id.slice(1, yui_gallery_id.length - 1).join('_');
 
 		console.log(yui_gallery_id);
 
