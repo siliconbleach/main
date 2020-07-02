240306e91c41f49084d09af590cfc25029b0cff8
Matthew McKey <mcstaplez@gmail.com>
2020-07-02 10:36:10 +0000

    Upload: scripts/artjam.js

--- scripts/artjam.js
+++ scripts/artjam.js
@@ -232,6 +232,7 @@ var js_cookie = createCommonjsModule(function (module, exports) {
 
   var toggleVote = function toggleVote(id) {
     var $voteSlide = $("#".concat(YUI_PREFIX).concat(yui_gallery_id).concat(id));
+    debugger;
     var styles = {
       background: 'white',
       color: '#e86d6d'
