7d11042231d04e7ef8d42b9b32a2429376febcdb
Matthew McKey <mcstaplez@gmail.com>
2020-07-02 08:05:38 +0000

    Upload: js/src/app.js

--- js/src/app.js
+++ js/src/app.js
@@ -101,8 +101,7 @@ import cookie from 'js-cookie';
 		}
 
 		if (typeof twitchIdFromCookie === 'string') {
-			const fetchedVotes = fetchVotes(twitchIdFromCookie);
-			console.log(fetchedVotes);
+			const fetchedVotes = fetchVotes(twitchIdFromCookie).then(res => res.json());
 		}
 
 	});
