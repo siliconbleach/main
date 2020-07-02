7c5b53a211ccebd3dfc386531901d03e02a9528b
Matthew McKey <mcstaplez@gmail.com>
2020-07-02 11:23:58 +0000

    Upload: js/src/app.js

--- js/src/app.js
+++ js/src/app.js
@@ -26,7 +26,7 @@ import cookie from 'js-cookie';
 		const vote = settings.votes.find(v => v.piece_id === id);
 		if (typeof vote !== 'undefined') {
 			debugger;
-			settings.votes.splice(vote, 1);
+			settings.votes.splice(vote, 1, 1);
 			debugger;
 			styles = {
 				background: 'transparent',
