cade81d7e5033dfa4674caf723f6010f3883c342
Matthew McKey <mcstaplez@gmail.com>
2020-07-02 16:37:16 +0000

    Upload: js/src/app.js

--- js/src/app.js
+++ js/src/app.js
@@ -58,7 +58,7 @@ import cookie from 'js-cookie';
 
 		const response = await fetch(`${API_URL}/api/votes`, {
 			method: 'POST',
-			body: settings,
+			body: JSON.stringify(settings),
 			headers: {
 				'Content-Type': 'application/json',
 			}
