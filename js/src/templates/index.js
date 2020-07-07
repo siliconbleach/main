4bda0546c225f2665bfdada5ba36c40d14daede6
Matthew McKey <mcstaplez@gmail.com>
2020-07-07 17:29:58 +0000

    Upload: js/src/templates/index.js

--- js/src/templates/index.js
+++ js/src/templates/index.js
@@ -1,15 +1,15 @@
 const INITIAL_TEMPLATE = `<div id="toastModal" class="js-is-hidden">
 <div class="card">
-   <div class="card-header"></div>
-
+   <div class="card-header">
+   {{headerContent}}</div>
    <div class="card-content">
-	<strong class={{statusClass}}> {{message}}</strong>
+   {{content}}
    </div>
 </div>
 </div>`;
 
 export default class Toast {
 	constructor(toastTemplate = INITIAL_TEMPLATE) {
-
+		this.template = toastTemplate;
 	}
 }
