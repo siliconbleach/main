1b837d44db6749197e590165417b6d93fc5f08c4
Matthew McKey <mcstaplez@gmail.com>
2020-07-28 12:51:29 +0000

    Upload: js/src/app.js

--- js/src/app.js
+++ js/src/app.js
@@ -117,7 +117,8 @@ import cookie from 'js-cookie';
 
 		const twitchIdFromCookie = cookie.get('userTwitchId');
 		if (typeof twitchIdFromCookie === 'string') {
-			fetchVotes(twitchIdFromCookie).then(res => res.json()).then(({ user: { votes, twitch_id, id, name } }) => {
+			fetchVotes(twitchIdFromCookie).then(res => res.json()).then(({ user }) => {
+				const { votes, twitch_id, id, name } = user;
 				console.log(votes);
 				settings.user = {
 					twitch_id,
