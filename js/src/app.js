bdf11d997913527846a5b90f60dbd8b4792992d7
Matthew McKey <mcstaplez@gmail.com>
2020-08-22 10:25:37 +0000

    Upload: js/src/app.js

--- js/src/app.js
+++ js/src/app.js
@@ -92,8 +92,7 @@ import cookie from 'js-cookie';
 			.then(data => {
 				const { success } = data;
 				if (success) {
-					toast.message = 'Vote received successfully!';
-					toast.success();
+					toast.success('Vote received successfully!');
 				}
 			});
 		return response;
