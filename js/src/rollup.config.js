e960726850fad23e738e2c113ccc797e9ed07027
Matthew McKey <mcstaplez@gmail.com>
2020-08-22 14:15:45 +0000

    Upload: js/src/rollup.config.js

--- js/src/rollup.config.js
+++ js/src/rollup.config.js
@@ -3,7 +3,6 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json'
 import svelte from 'rollup-plugin-svelte';
-import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 const production = !process.env.ROLLUP_WATCH;
