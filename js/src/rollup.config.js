a0d6d50ca3d88cf08b70b60d3fe5f33310b77e17
Matthew McKey <mcstaplez@gmail.com>
2020-08-22 14:17:29 +0000

    Upload: js/src/rollup.config.js

--- js/src/rollup.config.js
+++ js/src/rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import babel from '@rollup/plugin-babel';
-import json from '@rollup/plugin-json'
+import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json'
 import svelte from 'rollup-plugin-svelte';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
