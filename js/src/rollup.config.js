b3cc176dff79fc8329c2346addf5095066913b21
Matthew McKey <mcstaplez@gmail.com>
2020-08-22 13:40:04 +0000

    Upload: js/src/rollup.config.js

--- js/src/rollup.config.js
+++ js/src/rollup.config.js
@@ -2,6 +2,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json'
+import svelte from 'rollup-plugin-svelte';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
     input: './app.js',
