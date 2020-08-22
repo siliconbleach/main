7d7e965b8a8740411c2ff1c86d85a0eac0d1007b
Matthew McKey <mcstaplez@gmail.com>
2020-08-22 14:06:57 +0000

    Upload: js/src/rollup.config.js

--- js/src/rollup.config.js
+++ js/src/rollup.config.js
@@ -17,6 +17,15 @@ export default {
     plugins: [
         resolve(),
         commonjs(),
+        svelte({
+            // enable run-time checks when not in production
+            dev: !production,
+            // we'll extract any component CSS out into
+            // a separate file - better for performance
+            css: css => {
+                css.write('public/build/bundle.css');
+            }
+        }),
         babel({ babelHelpers: 'bundled' }),
         json({
             // All JSON files will be parsed by default,
