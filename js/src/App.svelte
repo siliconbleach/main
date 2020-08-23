eeab9958fdec863aa02bee178a15ea3810efd6fa
Matthew McKey <mcstaplez@gmail.com>
2020-08-23 11:21:09 +0000

    Upload: js/src/App.svelte

--- js/src/App.svelte
+++ js/src/App.svelte
@@ -1,9 +1,12 @@
 <script>
+  import { onMount } from "svelte/internal";
   import VoteManager from "./components/VoteManager.svelte";
   import FloatingSubmitButton from "./components/FloatingSubmitButton.svelte";
   export let user = {
     votes: []
   };
+
+  onMount(() => {});
 </script>
 
 <style lang="postcss">
