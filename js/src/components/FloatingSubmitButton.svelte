bd6e98e05ccdab503dd51b551fc9aa2c46d90300
Matthew McKey <mcstaplez@gmail.com>
2020-08-23 09:39:48 +0000

    Upload: js/src/components/FloatingSubmitButton.svelte

--- js/src/components/FloatingSubmitButton.svelte
+++ js/src/components/FloatingSubmitButton.svelte
@@ -1,9 +1,17 @@
 <script>
+  const MAX_VOTES_ALLOWED = 5;
+  export let votes;
 
+  $: votesRemaining = MAX_VOTES_ALLOWED - votes.length;
 </script>
 
 <style lang="postcss">
 
 </style>
 
-<form />
+<form>
+  <button id="submitvotes-button">
+    Submit Votes
+    <span />
+  </button>
+</form>
