ed3e012ffd049d4d51d7da800c609cde76c3d715
Matthew McKey <mcstaplez@gmail.com>
2020-08-23 11:47:19 +0000

    Upload: js/src/App.svelte

--- js/src/App.svelte
+++ js/src/App.svelte
@@ -10,31 +10,7 @@
 </script>
 
 <style lang="postcss">
-  .vote-container {
-    display: flex;
-    position: fixed;
-    bottom: 8px;
-    left: 8px;
-  }
 
-  .vote-holder {
-    width: 64px;
-    height: 64px;
-
-    border: 2px inset #fff;
-    border-radius: 6px;
-    margin: 0 8px;
-  }
-
-  @media screen and (min-width: 768px) {
-    .vote-container {
-      flex-direction: column;
-    }
-
-    .vote-holder {
-      margin: 8px auto;
-    }
-  }
 </style>
 
 <VoteManager {user} />
