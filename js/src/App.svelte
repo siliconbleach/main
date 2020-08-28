a4bf51b83882418ab65f0bcee16f3a9f995d966c
Matthew McKey <mcstaplez@gmail.com>
2020-08-28 20:40:42 +0000

    Upload: js/src/App.svelte

--- js/src/App.svelte
+++ js/src/App.svelte
@@ -2,9 +2,19 @@
   import { onMount } from "svelte/internal";
   import VoteManager from "./components/VoteManager.svelte";
 
+  const API_URL = "https://artofkoko.com";
   export let user = {
     votes: Array(5)
   };
+
+  const getPieces = async () => {
+    const response = await fetch(`${API_URL}/api/artjam`).then(res =>
+      res.json()
+    );
+    return response;
+  };
+
+  onMount(() => {});
 </script>
 
 <style lang="postcss">
