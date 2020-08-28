6c7eba2c7cd8af1f2910f9c95e856394cc245ae2
Matthew McKey <mcstaplez@gmail.com>
2020-08-28 21:53:36 +0000

    Upload: js/src/components/ContestGallery.svelte

---
+++ js/src/components/ContestGallery.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<style lang="postcss">
+
+</style>
+
+<div class="artjam-gallery-container">
+  {#each contest.entries as entry}
+    <span>{entry.name} - {entry.artist}</span>
+  {/each}
+</div>
