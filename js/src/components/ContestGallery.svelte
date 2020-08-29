226b54723672c919140955e4a18756f9bac99587
Matthew McKey <mcstaplez@gmail.com>
2020-08-29 12:07:06 +0000

    Upload: js/src/components/ContestGallery.svelte

--- js/src/components/ContestGallery.svelte
+++ js/src/components/ContestGallery.svelte
@@ -4,11 +4,63 @@
     entries: [],
   };
 
+  $: contest = $$props.contest;
+
   const dispatch = createEventDispatcher();
 
   const toggleVote = (entry) => {
     console.log("Toggle-vote being dispatched");
+    debugger;
     dispatch("togglevote", { entry });
+
+    debugger;
+  };
+</script>
+
+<style lang="postcss">
+  .artjam-gallery-container {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    align-items: center;
+    max-width: 960px;
+  }
+
+  .artjam-entry {
+    width: 100%;
+    max-width: 23vw;
+    background-color: lightslategray;
+    color: white;
+    border: 1px solid transparent;
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+    margin: 1rem auto;
+  }
+</style>
+
+<div class="artjam-gallery-container">
+  {#each contest?.entries as entry}
+    <button class="artjam-entry" on:click={() => toggleVote(entry)}>
+      {entry.name}
+    </button>
+  {/each}
+</div>
+<script>
+  import { createEventDispatcher } from "svelte";
+  export let contest = {
+    entries: [],
+  };
+
+  $: contest = $$props.contest;
+
+  const dispatch = createEventDispatcher();
+
+  const toggleVote = (entry) => {
+    console.log("Toggle-vote being dispatched");
+    debugger;
+    dispatch("togglevote", { entry });
+
+    debugger;
   };
 </script>
 
