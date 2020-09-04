79f02b166aadcf062ccafa98df08c9de2abd7156
Matthew McKey <mcstaplez@gmail.com>
2020-09-04 12:41:35 +0000

    Upload: js/src/components/GalleryLightbox.svelte

---
+++ js/src/components/GalleryLightbox.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let activeImg = null;
+</script>
+
+<style lang="postcss">
+
+</style>
+
+<div class="gallery-lightbox-overlay" />
+<div class="gallery-lightbox">
+  <picture>
+    <img src={activeImg && activeImg.src} alt="Text for the alt tag" />
+  </picture>
+</div>
