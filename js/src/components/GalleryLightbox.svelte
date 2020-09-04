d2d0b33a427219d1e7fd3cf766e67e639c5643d0
Matthew McKey <mcstaplez@gmail.com>
2020-09-04 12:43:17 +0000

    Upload: js/src/components/GalleryLightbox.svelte

--- js/src/components/GalleryLightbox.svelte
+++ js/src/components/GalleryLightbox.svelte
@@ -3,7 +3,23 @@
 </script>
 
 <style lang="postcss">
+  .gallery-lightbox-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vw;
+    z-index: 9999;
+    background: rgba(0, 0, 0, 0.75);
+    opacity: 0;
+    visibility: hidden;
+    transition: all 0.75s ease-in-out;
 
+    &.is-open {
+      opacity: 1;
+      visibility: visible;
+    }
+  }
 </style>
 
 <div class="gallery-lightbox-overlay" />
