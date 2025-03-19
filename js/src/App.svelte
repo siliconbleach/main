e164afa7fb79b0cbd5fb4b51e0aef3654b123879
Matthew McKey <mcstaplez@gmail.com>
2025-03-19 16:41:24 +0000

    Remove all sorts of shit

--- js/src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  console.log("I should be working now");
-  import FullscreenGallery from "./components/FullscreenGallery.svelte";
-</script>
-
-<style lang="postcss">
-</style>
-
-<FullscreenGallery />
